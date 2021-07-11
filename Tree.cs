c4253a98c92cefd9eab1acbb6135097b2504482e
OmarAbdullah <1700884@eng.asu.edu.eg>
2021-07-11 17:17:54 +0200

    Node,Tree

---
+++ Tree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XML_editor
+{
+    class Tree
+    {
+       private Node root;
+        Tree(ref Node n)
+        {
+            root = n;
+        }
+        public void insert() { }
+        public void format() { }
+        public void conv2JSON() { }
+
+    }
+}
