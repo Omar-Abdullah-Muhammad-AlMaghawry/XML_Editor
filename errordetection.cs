58803a329f5d03d23a316f313e48751145ac31e9
Abdurrahman Eshibiny <a.eshibiny@gmail.com>
2021-07-14 00:07:16 +0200

    added Mina's error detection

--- errordetection.cs
+++ errordetection.cs
@@ -1,88 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace XML_editor
 {
-    public partial class Form1 : Form
+    class error
     {
-        public Form1()
+        public static bool errordetection(string mystring)
         {
-            InitializeComponent();
-        }
-
-        private void richTextBox2_TextChanged(object sender, EventArgs e)
-        {
-            if (!errordetection(richTextBox2.Text))
-            {
-                Node root = new Node();
-                Tree tree = new Tree(ref root, richTextBox2.Text);
-            }
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            errordetection(richTextBox2.Text);
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-
-            LinkedList<int> er = new LinkedList<int>();
-
-            Queue<string> q = new Queue<string>();
-            q.Enqueue("ref");
-            q.Enqueue("hhtttp **");
-            List<Node> l = new List<Node>();
-            l.Add(new Node("n1","gvflgklflkg"));
-            l.Add(new Node("n2", "ooooooooooooooo"));
-            l.Add(new Node("n1", "ddddddddddddddd"));
-            l.Add(new Node("n3", "mmmmmmmmmmmmmm"));
-            Node n = new Node("span",ref q,ref l, "llllllllllllllllll");
-            Tree x = new Tree(ref n);
-            x.conv2Json(ref n, ref er, -1);
-            richTextBox2.Text = x.getJSON();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        public bool errordetection(string mystring)
-        {
-            richTextBox1.Text = "";
             string storee = "";
             string checkk = "";
             int counter = 1;
@@ -141,22 +68,17 @@ namespace XML_editor
                 {
                     while (myStack.Count != 0 && counterer.Count != 0)
                     {
-                        //Console.WriteLine("error occur at line " + counterer.Pop());
-                        richTextBox1.Text += "Error occur at line " + counterer.Pop();
+                        Console.WriteLine("error occur at line " + counterer.Pop());
                     }
                 }
                 else
-                {
-                    //Console.WriteLine("error occur at line " + counterer.Pop());
-                    richTextBox1.Text += "Error occur at line " + counterer.Pop();
-                }
-
+                    Console.WriteLine("error occur at line " + counterer.Pop());
+                
                 return true;
             }
             else
             {
-                //Console.WriteLine("no error");
-                richTextBox1.Text += "No error";
+                Console.WriteLine("no error");
                 return false;
             }
             //Console.Read();
