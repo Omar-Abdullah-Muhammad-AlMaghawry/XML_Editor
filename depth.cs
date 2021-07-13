2b4141cdc9102e6eac186d07d255ea18e5b2896b
nahlaayman <73783592+nahlaayman@users.noreply.github.com>
2021-07-13 19:22:44 +0200

    Add files via upload

---
+++ depth.cs
@@ -0,0 +1,111 @@
+
+        public int Depth( ref Node n, ref Node s)
+        {
+            if(n.getName()  ==  s.getName()) 
+            {
+                return 0;
+            }
+            else
+            {
+                 
+                  if (s.getCountCh() != 0)
+                  {
+                      ++depth;
+                  }
+                 List<Node> data =s.getAllCh();
+        
+                 for(int i=0; i<s.getCountCh();i++)
+                  {  
+                   if(data[i].getName() == n.getName()) return depth;
+                  } 
+                 
+                 for(int j=0;j<s.getCountCh();j++)
+                  {
+                  Node d=data[j];
+                  if(s.getCountCh()!=0)
+                   Depth(ref n, ref d);  //error data[j] 
+        
+                  }
+                  return depth;
+            }
+   
+
+        }
+      public void Space(Node k)
+      {
+         int n=0;
+         depth=0;
+         n=Depth( ref k,ref root);
+          // Console.Write(n);
+         for(int i=0;i<=n;i++)
+          {
+            Console.Write(" ");
+          }
+      }
+      public void print(Node r ) 
+      { 
+          Space(r);
+          Console.Write("<"+r.getName()+" "+r.getOneAttr()+">");
+          Console.Write(r.getValue());
+          List<Node> child =r.getAllCh();
+          if (r.getCountCh()!=0)
+           {
+              Console.Write("\n");
+             for(int i=0;i<r.getCountCh();i++)
+             {
+              print(child[i]);
+             }
+           }
+          if(r.getCountCh()!=0)
+             Space(r);
+          Console.Write("</"+r.getName()+">");
+          Console.WriteLine("\n");
+       
+        }
+
+      
+      public void format()
+        {
+			print(root);
+		}
+
+         
+
+      public void Mini(Node k)
+      {
+        
+          
+          Console.Write( "<"+k.getName());
+         // +" "+k.getOneAttr()+">");//getting all atter.
+
+          if(k.getCountAttr()!=0)
+          {
+              Console.Write(" ");
+             foreach(string s in k.getAllAttr())
+             {
+              Console.Write(s+" ");
+             }
+         }
+          Console.Write(k.getValue());
+		   List<Node> child =k.getAllCh();
+          if (k.getCountCh() !=0)
+          {
+        
+             for(int i=0;i<k.getCountCh();i++)
+               {
+                 Mini(child[i]);
+               }
+          
+           }
+
+          
+          Console.Write("</"+k.getName()+">");
+           
+            
+   
+
+      } 
+      public void Minifying()
+        {
+               Mini(root);
+        } 
