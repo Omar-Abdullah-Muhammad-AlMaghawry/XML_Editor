b7f2d223c60565eb6a5f86500577a0f1a0637131
OmarAbdullah <1700884@eng.asu.edu.eg>
2021-07-15 11:48:03 +0200

    vision 1.0.10 for merged , improved convert2json for ,  ... thanks Allah

--- Tree.cs
+++ Tree.cs
@@ -451,7 +451,7 @@ namespace XML_editor
                         for (int t = 0; t < depth; t++)
                             json = json + "\t";
                        
-                        if(!r.getIsLastLast())
+                        if((r == father.getAllCh()[father.getCountCh() - 1]) || !r.getIsLastLast())
                             json = json + "]," + "\n";
                         else json = json + "]" + "\n";
                         // e.Dequeue();
@@ -465,7 +465,7 @@ namespace XML_editor
             if (r.getCountCh() == 0)
             {
                 // if(((!r.getIsFirst() || !r.getIsFirstFirst()) && r.getRepeated() )|| ((r.getIsLast())&&r.getRepeated())|| (r.getIsLastLast()))
-               if( ((r.getIsLast())&&r.getRepeated())|| (r.getIsLastLast()))
+               if( ((r==father.getAllCh()[father.getCountCh()-1])||(r.getIsLast())&&r.getRepeated())|| (r.getIsLastLast()))
                 json = json + "}\n";
                 else if (r.getIsFirst()||r.getIsFirstFirst()||!r.getRepeated())
                     json = json + "},\n";
